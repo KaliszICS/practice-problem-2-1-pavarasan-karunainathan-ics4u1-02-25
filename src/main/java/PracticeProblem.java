199bb86698dabeda3fbbe50a0abf6c268a225bb3
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-03-18 13:33:35 +0000

    fix

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,35 @@
-public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+public class PracticeProblem{
+	static int find(int[] array, int number){
+		for(int i = 0; i < array.length; ++i){
+			if(array[i] == number){
+				return i;
+			}
+		}
+		return -1;
 	}
 
-	
+	static int findLast(String[] array, String string){
+		for(int i = array.length-1; i > -1; --i){
+			if(array[i] == string){
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	static int findSecond(char[] array, char character){
+		boolean first = false;
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == character){
+				if(!first){
+					first = true;
+				}else{
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String args[]){}
 }
